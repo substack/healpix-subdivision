a890f3643ac37932b48043ec284bb20174f42d7b
James Halliday <mail@substack.net>
2016-07-23 03:55:48 -0700

    grid shapes, graphs visually check out

--- example/grid.js
+++ example/grid.js
@@ -1,2 +1,4 @@
 var grid = require('../')(4,3)
-console.log(grid())
+var data = grid()
+//data = data.concat(grid(data[0]))
+console.log(JSON.stringify(data,null,2))
--- index.js
+++ index.js
@@ -5,30 +5,37 @@ module.exports = function (H,K) {
   var hp = healpix(H,K)
   return function (bbox) {
     if (!bbox) return regions(hp,H,K)
-    else {
-      console.log('todo')
-    }
+    console.log('todo')
   }
 }
 
 function regions (hp,H,K) {
-  var thetax = Math.asin((K-1)/K)
   var rs = []
-  for (var i = 0; i < H; i++) {
-    for (var j = 0; j < K; j++) {
+  var theta = 1
+  var shapes
+  for (var j = 0; j < K; j++) {
+    var tstep = thetaStep(j)
+    for (var i = 0; i < H; i++) {
+      if (j > 0 && j < K-1) rs.push(shapes = [])
+      else if (i === 0) rs.push(shapes = [])
       var w = (i/H*2-1)*PI
       var e = ((i+1)/H*2-1)*PI
-      var s = Math.asin((K-(j+1)*(j+2)/2)/K)
-      var n = Math.asin((K-j*(j+1)/2)/K)
-      var pt0 = hp.ang2xy([],w,s)
-      rs.push([
-        pt0,
-        hp.ang2xy([],e,s),
-        hp.ang2xy([],e,n),
-        hp.ang2xy([],w,n),
-        [pt0[0],pt0[1]]
-      ])
+      var s = Math.asin(theta-tstep)
+      var n = Math.asin(theta)
+      if (j === 0) {
+        shapes.push([ [w,s], [e,s], [(e+w)/2,n], [w,s] ])
+      } else if (j === K-1) {
+        shapes.push([ [w,n], [e,n], [(e+w)/2,s], [w,n] ])
+      } else {
+        shapes.push([ [w,s], [e,s], [e,n], [w,n], [w,s] ])
+      }
     }
+    theta -= tstep
   }
   return rs
+
+  function thetaStep (i) {
+    var k=(K-1)/2
+    return Math.pow(k-Math.abs(k-i)+1,2)/3
+  }
 }
