c1d5f67e738109890b7270a64d73e16e24f868df
James Halliday <mail@substack.net>
2016-07-25 00:00:08 -0700

    4-in-one polar geometry

--- example/grid.js
+++ example/grid.js
@@ -1,6 +1,4 @@
 var grid = require('../')(4)
 var data = grid()
-data = data.concat(grid(data[2]))
-//data = data.concat(grid(data[7]))
-data = data.concat(grid(data[5]))
+//data = data.concat(grid(data[5]))
 console.log(JSON.stringify(data,null,2))
--- index.js
+++ index.js
@@ -19,18 +19,48 @@ module.exports = function (size) {
         exn = Math.max(exn,s[i][1])
       }
       var sx = (exe-exw)/size
-      var yscale = 1
-      if (Math.abs(exn)-EPSILON > thetax) yscale = 2
-      else if (Math.abs(exs)-EPSILON > thetax) yscale = 2
-      var sy = (exn-exs)/size * yscale
+      var polar = Math.abs(exn)-EPSILON > thetax
+        || Math.abs(exs)-EPSILON > thetax
+      var sy = (exn-exs)/size //* yscale
 
-      for (var y = 0; y < size; y++) {
-        for (var x = 0; x < size; x++) {
-          var w = exw + sx*x
-          var s = exs + sy*y - sy*size/2 * (yscale-1)
-          var e = exw + sx*(x+1)
-          var n = exs + sy*(y+1) - sy*size/2 * (yscale-1)
-          parts.push([ [ [w,s], [e,s], [e,n], [w,n], [w,s] ] ])
+      if (polar) {
+        for (var y = 0; y < size/2; y++) {
+          var y0 = s[0][1] + y*sy*2
+          var y1 = s[0][1] + (y+1)*sy*2
+          parts.push([
+            [
+              [s[0][0],y0],
+              [s[0][0],y1],
+              [s[2][0],y1],
+              [s[0][0],y0]
+            ]
+          ])
+          /*
+          parts.push([
+            [
+              [-PI*(3/4-1/8),-thetax],
+              [-PI*(3/4-1/8),-thetax - sy*2],
+              [-PI*(3/4-1/4),-thetax],
+              [-PI*(3/4-1/8),-thetax]
+            ],
+            [
+              [-PI*(1/4+1/4),-thetax],
+              [-PI*(1/4+1/8),-thetax - sy*2],
+              [-PI*(1/4+1/8),-thetax],
+              [-PI*(1/4+1/4),-thetax]
+            ],
+          ])
+          */
+        }
+      } else {
+        for (var y = 0; y < size; y++) {
+          for (var x = 0; x < size; x++) {
+            var w = exw + sx*x
+            var s = exs + sy*y
+            var e = exw + sx*(x+1)
+            var n = exs + sy*(y+1)
+            parts.push([ [ [w,s], [e,s], [e,n], [w,n], [w,s] ] ])
+          }
         }
       }
     })
@@ -45,16 +75,23 @@ function regions (H,K) {
   for (var j = 0; j < K; j++) {
     var tstep = thetaStep(j)
     for (var i = 0; i < H; i++) {
-      if (j > 0 && j < K-1) rs.push(shapes = [])
-      else if (i === 0) rs.push(shapes = [])
+      if (j > 0 && j < K-1) rs.push(shapes = [[]])
+      else if (i === 0) rs.push(shapes = [[]])
       var w = (i/H*2-1)*PI
       var e = ((i+1)/H*2-1)*PI
       var s = Math.asin(theta-tstep)
       var n = Math.asin(theta)
-      if (j === 0) {
-        shapes.push([ [w,s], [e,s], [(e+w)/2,n], [w,s] ])
-      } else if (j === K-1) {
-        shapes.push([ [w,n], [e,n], [(e+w)/2,s], [w,n] ])
+      if (j === 0 || j === K-1) {
+        var xn = j === 0 ? n : s
+        var xs = j === 0 ? s : n
+        shapes[shapes.length-1].push(
+          [w,xs],
+          [(e+w)/2,xn],
+          [e,xs]
+        )
+        if (i === H-1) {
+          shapes[shapes.length-1].push(shapes[shapes.length-1][0])
+        }
       } else {
         shapes.push([ [w,s], [e,s], [e,n], [w,n], [w,s] ])
       }
@@ -67,4 +104,17 @@ function regions (H,K) {
     var k=(K-1)/2
     return Math.pow(k-Math.abs(k-i)+1,2)/3
   }
+}
+
+function lengths (n) {
+  var lens = []
+  for (var y = 0; y < n/2; y++) {
+    var row = []
+    lens.push(row)
+    for (var x = 0; x < 4; x++) {
+      if (y > 0) row.push(y, n-y*2, y)
+      else row.push(n-y*2)
+    }
+  }
+  return lens
 }
