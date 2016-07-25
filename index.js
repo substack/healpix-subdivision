d9c263e30f6acb06db047862cf57f169fa309447
James Halliday <mail@substack.net>
2016-07-25 03:52:44 -0700

    splitting tops works except for sub-case has offset issue

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var almost = require('almost-equal')
 var H = 4, K = 3
 var hp = healpix(H,K)
 var PI = Math.PI
+var EPSILON = 1e-8
 
 module.exports = function (size) {
   return function (shapes) {
@@ -12,13 +13,11 @@ module.exports = function (size) {
     var exw = s[0][0], exs = s[0][1], exe = s[0][0], exn = s[0][1]
     var sx, sy
 
-    if (shapes.length === 1 && s.length === 10) {
-      splitTop(parts, s, size)
+    if (shapes.length === 4) { // tops
+      splitTop(parts, shapes, size)
     } else if (shapes.length === 2) { // 2-tri
       splitTri(parts, shapes, size)
-    } else if (shapes.length === 4) { // tops
-      //splitTop(parts, shapes, size)
-    } else {
+    } else if (shapes.length === 1) {
       getex()
       for (var y = 0; y < size; y++) {
         for (var x = 0; x < size; x++) {
@@ -29,6 +28,8 @@ module.exports = function (size) {
           parts.push([ [ [w,s], [e,s], [e,n], [w,n], [w,s] ] ])
         }
       }
+    } else {
+      console.error('unhandled')
     }
     return parts
 
@@ -45,19 +46,20 @@ module.exports = function (size) {
   }
 }
 
-function splitTop (parts, s, size) {
-  var sx = (s[s.length-2][0]-s[0][0])/size
+function splitTop (parts, shapes, size) {
+  var s = shapes[0]
+  var sx = (s[0][0]-s[1][0])/size
   var sy = (s[0][1]-s[1][1])/size
   var yn = s[0][1] > 0 ? -1 : 1
   var z = size/2, fz = Math.floor(z), q = (s[2][0]-s[1][0])/z
   var tops = null
   if (size % 2 === 1) parts.push(tops = [])
-  for (var j = 1; j < 9; j += 2) {
+  for (var j = 0; j < shapes.length; j++) {
     for (var y = 0; y < z; y++) {
       if (y === fz && tops) {
         var y0 = s[0][1] - y*sy*2 * yn
         var y1 = y0 - sy * yn
-        var x0 = s[j][0] - (s[2][0]-s[1][0])*(1-y/z)
+        var x0 = shapes[j][1][0] - (s[2][0]-s[1][0])*(1-y/z)
         var x1 = x0 + q/2
         var x2 = x0 + q
         tops.push([ [x0,y0], [x1,y1], [x2,y0], [x0,y0] ])
@@ -65,15 +67,15 @@ function splitTop (parts, s, size) {
       }
       var y0 = s[0][1] - y*sy*2 * yn
       var y1 = s[0][1] - (y+1)*sy*2 * yn
-      var x0 = s[j][0] + (s[2][0]-s[1][0])*(1-y/z) - q
+      var x0 = shapes[j][1][0] + (s[2][0]-s[1][0])*(1-y/z) - q
       var x1 = x0 + q
-      var x2 = s[j+2][0] - (s[2][0]-s[1][0])*(1-y/z) + q
+      var x2 = shapes[j][2][0] - (s[2][0]-s[1][0])*(1-y/z) + q*(size+2)/2
       var x3 = x2 - q
       parts.push([
         [ [x0,y1], [x0,y0], [x1,y0], [x0,y1] ],
         [ [w(x2),y1], [w(x2),y0], [w(x3),y0], [w(x2),y1] ]
       ])
-      for (var k = 0; k < (fz-y-1)*2; k++) {
+      for (var k = 0; k < (z-y-1)*2; k++) {
         var sq = [
           [x0-k*q,y0],
           [x0-(k+1)*q,y0],
@@ -85,7 +87,9 @@ function splitTop (parts, s, size) {
       }
     }
   }
-  function w (x) { return x <= -PI ? PI/4+x : x }
+  function w (x) {
+    return x+EPSILON >= PI ? x-2*PI : x
+  }
 }
 
 function splitTri (parts, shapes, size) {
@@ -150,24 +154,13 @@ function regions (H,K) {
       if (j === 0 || j === K-1) {
         var xn = j === 0 ? n : s
         var xs = j === 0 ? s : n
-        if (i === 0) {
-          shapes.push([[
-            [w,xs],
-            [(e+w)/2,xn],
-            [e,xs]
-          ]])
-        } else if (i === H-1) {
-          shapes[shapes.length-1][0].push(
-            [(e+w)/2,xn],
-            [e,xs],
-            shapes[shapes.length-1][0][0]
-          )
-        } else {
-          shapes[shapes.length-1][0].push(
-            [(e+w)/2,xn],
-            [e,xs]
-          )
-        }
+        if (i === 0) shapes.push([])
+        shapes[shapes.length-1].push([
+          [w,xs],
+          [(e+w)/2,xn],
+          [e,xs],
+          [w,xs]
+        ])
       } else {
         shapes.push([ [ [w,s], [e,s], [e,n], [w,n], [w,s] ] ])
       }
