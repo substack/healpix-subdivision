089a8f7932dd9b69ee2a97bf10b86a45ad258507
James Halliday <mail@substack.net>
2016-07-25 01:15:46 -0700

    first polar edge triangles works at size=4

--- example/grid.js
+++ example/grid.js
@@ -1,4 +1,5 @@
 var grid = require('../')(4)
 var data = grid()
-//data = data.concat(grid(data[5]))
+//data = data.concat(grid(data[3]))
+data = data.concat(grid(data[5]))
 console.log(JSON.stringify(data,null,2))
--- index.js
+++ index.js
@@ -23,35 +23,10 @@ module.exports = function (size) {
         || Math.abs(exs)-EPSILON > thetax
       var sy = (exn-exs)/size //* yscale
 
-      if (polar) {
-        for (var y = 0; y < size/2; y++) {
-          var y0 = s[0][1] + y*sy*2
-          var y1 = s[0][1] + (y+1)*sy*2
-          parts.push([
-            [
-              [s[0][0],y0],
-              [s[0][0],y1],
-              [s[2][0],y1],
-              [s[0][0],y0]
-            ]
-          ])
-          /*
-          parts.push([
-            [
-              [-PI*(3/4-1/8),-thetax],
-              [-PI*(3/4-1/8),-thetax - sy*2],
-              [-PI*(3/4-1/4),-thetax],
-              [-PI*(3/4-1/8),-thetax]
-            ],
-            [
-              [-PI*(1/4+1/4),-thetax],
-              [-PI*(1/4+1/8),-thetax - sy*2],
-              [-PI*(1/4+1/8),-thetax],
-              [-PI*(1/4+1/4),-thetax]
-            ],
-          ])
-          */
-        }
+      if (s.length === 10 && polar) {
+        firstPolar(parts, s, size, sx, sy)
+      } else if (polar) {
+        // ...
       } else {
         for (var y = 0; y < size; y++) {
           for (var x = 0; x < size; x++) {
@@ -68,15 +43,34 @@ module.exports = function (size) {
   }
 }
 
+function firstPolar (parts, s, size, sx, sy) {
+  var yn = s[0][1] > 0 ? -1 : 1
+  var z = size/2
+  for (var j = 1; j < 9; j += 2) {
+    for (var y = 0; y < z; y++) {
+      var y0 = s[0][1] - y*sy*2 * yn
+      var y1 = s[0][1] - (y+1)*sy*2 * yn
+      var x0 = s[j][0] + (s[2][0]-s[1][0])*(1/2-y/size*2)
+      var x1 = x0 + (s[2][0]-s[1][0])/2
+      var x2 = s[j+2][0] - (s[2][0]-s[1][0])*(1/2-y/size*2)
+      var x3 = x2 - (s[2][0]-s[1][0])/2
+      parts.push([
+        [ [x0,y1], [x0,y0], [x1,y0], [x0,y1] ],
+        [ [w(x2),y1], [w(x2),y0], [w(x3),y0], [w(x2),y1] ]
+      ])
+    }
+  }
+  function w (x) {
+    return x <= -PI ? PI/4+x : x
+  }
+}
+
 function regions (H,K) {
-  var rs = []
+  var shapes = []
   var theta = 1
-  var shapes
   for (var j = 0; j < K; j++) {
     var tstep = thetaStep(j)
     for (var i = 0; i < H; i++) {
-      if (j > 0 && j < K-1) rs.push(shapes = [[]])
-      else if (i === 0) rs.push(shapes = [[]])
       var w = (i/H*2-1)*PI
       var e = ((i+1)/H*2-1)*PI
       var s = Math.asin(theta-tstep)
@@ -84,21 +78,31 @@ function regions (H,K) {
       if (j === 0 || j === K-1) {
         var xn = j === 0 ? n : s
         var xs = j === 0 ? s : n
-        shapes[shapes.length-1].push(
-          [w,xs],
-          [(e+w)/2,xn],
-          [e,xs]
-        )
-        if (i === H-1) {
-          shapes[shapes.length-1].push(shapes[shapes.length-1][0])
+        if (i === 0) {
+          shapes.push([[
+            [w,xs],
+            [(e+w)/2,xn],
+            [e,xs]
+          ]])
+        } else if (i === H-1) {
+          shapes[shapes.length-1][0].push(
+            [(e+w)/2,xn],
+            [e,xs],
+            shapes[shapes.length-1][0][0]
+          )
+        } else {
+          shapes[shapes.length-1][0].push(
+            [(e+w)/2,xn],
+            [e,xs]
+          )
         }
       } else {
-        shapes.push([ [w,s], [e,s], [e,n], [w,n], [w,s] ])
+        shapes.push([ [ [w,s], [e,s], [e,n], [w,n], [w,s] ] ])
       }
     }
     theta -= tstep
   }
-  return rs
+  return shapes
 
   function thetaStep (i) {
     var k=(K-1)/2
